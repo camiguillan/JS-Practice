f3120eb10ed4d25e5b6c8950ffdcb6285296c3d6
camiguillan <camiguillan@gmail.com>
2023-03-02 19:08:07 -0300

    added local storage

--- ToDoList/toDo.js
+++ ToDoList/toDo.js
@@ -1,3 +1,49 @@
+(function() { 
+    var keys = Object.keys(localStorage), i ,
+    tasks =[];
+    // while(i--){
+    //     tasks.push(localStorage.getItem("keys[i]"));
+    //     document.getElementById('list').textContent = tasks;
+    // }
+
+    for(i=0; i<keys.length; i++){
+        var value = String(localStorage.getItem(keys[i]));
+        console.log(value);
+        insertToList(value);
+    }
+       
+
+})();  
+
+
+function addToLocalStorage(todo){
+    var keys = Object.keys(localStorage), i ,
+    tasks =[], found;
+
+    for(i=0; i<keys.length; i++){
+        const value = localStorage.getItem(keys[i]);
+        if(todo == value ){
+            found = true;
+        }
+    }
+
+
+    if(!found){
+        window.localStorage.setItem(keyGenerator(), todo);
+    }
+
+}
+
+
+var task = 0;
+
+function keyGenerator(){
+    const newKey = 'task' + task;
+    task ++;
+    return newKey;
+}
+
+
 function addToDo(){
     var toDo = document.getElementById("newToDo").value;
     document.getElementById("newToDo").value = "";
@@ -6,17 +52,20 @@ function addToDo(){
 
 //INSERT A NEW ELEMENT TO A HTML LIST
 function insertToList(toDo){
-    var item = createLi(toDo);
+    console.log(toDo);
+    var item = createLi();
     var checkBox = createCheck();
     var label = createLabel();
+    var but = createDelete(); 
     
     item.appendChild(label);
     label.appendChild(checkBox);
     label.appendChild(document.createTextNode(toDo));
-    
-    
-    //createDelete(item);    
+    label.appendChild(but); 
     document.getElementById("list").appendChild(item);
+    //window.localStorage.setItem(keyGenerator(), item.innerHTML);
+    addToLocalStorage(toDo);
+ 
     
   
 }
@@ -40,18 +89,57 @@ function createCheck(){
     return check;
 }
 
-/*
-function createDelete(item){
+
+function createDelete(){
     var buttom = document.createElement("button");
     buttom.appendChild(document.createTextNode('Delete'));
     buttom.className ='delete';
-    buttom.onclick = "deleteItem()";
-    item.appendChild(buttom);
+    buttom.onclick = function(){
+        var label = buttom.parentNode;
+        var itemToDelete = label.parentNode;
+        var ul = itemToDelete.parentNode;
+        ul.removeChild(itemToDelete);
+    }
+    ;
+    //buttom.addEventListener("click",deleteItem(buttom)) ;
+    return buttom;
 }
 
-var deleteItem = function(){
-    var itemToDelete = this.parentNode;
-    var ul = itemToDelete.parentNode
+
+
+function deleteItem(buton){
+    var label = buton.parentNode;
+    var itemToDelete = label.parentNode;
+    var ul = itemToDelete.parentNode;
     ul.removeChild(itemToDelete);
 }
-*/
+
+function editItem(editBut){
+        var label = editBut.parentNode, task;
+        task = searchChild(label, 'LABEL')
+        console.log(task);
+        task.contentEditable = true;
+        task.style.backgroundColor = "#dddbdb";
+        task.innerHTML = 'hola';
+        changeButtons(label, 'LABEL');
+    
+}
+
+
+function searchChild(parent, c){
+    for (const child of parent.children) {
+        console.log(child.tagName);
+        if(child.tagName == c){
+            task = child;
+        }
+      }
+    
+    return task; 
+}
+
+// function changeButtons(label){
+//     var dele = searchChild(label, 'BUTTON');
+//     dele.appendChild(document.createTextNode('Cancel'));
+//     var edit = searchChild(label, 'BUTTON');
+//     edit.appendChild(document.createTextNode('Save'));
+// }
