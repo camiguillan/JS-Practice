742d2dd7b9d725e7428b7abe554c7b722e96f225
camiguillan <camiguillan@gmail.com>
2023-03-15 11:16:11 -0300

    Day 0-1

--- REACT/myapp/src/App.js
+++ REACT/myapp/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+   
+        <p className="App-header">
+          HELLO WORLD!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     
+
     </div>
   );
 }
-
+//lets you mark the main function in a file so that you can later import it from other files
+//their names must start with a capital letter or they won’t work!
 export default App;
+
+
