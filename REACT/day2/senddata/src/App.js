b86e56ce7d2329cbb192672e99eb6b49eeae640a
camiguillan <camiguillan@gmail.com>
2023-03-20 17:22:46 -0300

    passing data between components

--- REACT/day2/senddata/src/App.js
+++ REACT/day2/senddata/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import Receive from './reciever';
+import './App.css';
 
 function SendData() {
   const [name, setName] = useState('');
   const [surname, setSurname] = useState('');
   const [post, setPost] = useState('');  
+  const [showBool, setShow] = useState(false);  
   // var data = {name: "camila",surname:"hola",post:"dasdas"};
-  var data = {};
+  var data = {    
+    name,
+    surname,
+    post};
+  var show = {showBool}
 
   function saveData(){
   data = {
@@ -15,32 +21,42 @@ function SendData() {
       surname,
       post
     };
+
   }
 
-  async function handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
     saveData();
     console.log('Datos:', data);       
-    
+    setShow(!showBool);
+
   };
 
 
     return (
       <div id="div">
-        <p>Simple input box</p>
+        <h1>Posts</h1>
+
+        <div>
         Name: <input type="text" placeholder="Type here" value = {name}
         onChange={(event) => setName(event.target.value)}></input>
         <br></br>
-
+        </div>
+       
+      <div>
         Surname: <input type="text" placeholder="Type here"  value= {surname}
         onChange={(event) => setSurname(event.target.value)}></input>
         <br></br>
+        </div>
 
+
+        <div>
         Post: <input type="text" placeholder="Type here" value= {post}
         onChange={(event) => setPost(event.target.value)}></input>
+        </div>
 
         <button onClick={handleSubmit}>Send Data</button>
-        <Receive d= {data}/>
+        <Receive d= {data} s= {show}/>
               
         
       </div>
--- REACT/day2/senddata/src/reciever.js
+++ REACT/day2/senddata/src/reciever.js
@@ -2,20 +2,31 @@ import React from 'react';
 import { useEffect } from 'react';
 
 
-function Receive(props) {    
-  useEffect(() => {
-    console.log('data changed:', props.d);
-  }, [props.d]);
-  return (
-    <div>
-      <p>Name: {props.d.name}</p>
-      <p>Surname: {props.d.surname}</p>
-      <p>Post: {props.d.post}</p>
-    </div>
-  );
-   
+function Receive(props) {  
+  const show = props.s.showBool;
+  console.log("Show is ", show);
+  if(show == true){    
+    return (
+      
+      <div>
+        <p>Name: {props.d.name}</p>
+        <p>Surname: {props.d.surname}</p>
+        <p>Post: {props.d.post}</p>
+      </div>
+    );
+    
 
+    }
+    else
+    return (
+      <div>
+      <p>Name: </p>
+      <p>Surname:</p>
+      <p>Post: </p>
+    </div>
+    )
   }
+
 
 export default Receive;
 
