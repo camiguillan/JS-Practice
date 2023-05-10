0d7232a1927ca3aab341fe58645e53a1a0288d12
camiguillan <camiguillan@gmail.com>
2023-05-09 21:09:15 -0300

    changes for onboarding

---
+++ REACT/react-app/src/components.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './components-styles.css';
+
+export default function Components() {
+  return (
+    <div className='cont'>
+        <h1>An interesting app</h1>
+        <div className='comps'> 
+            <p>Hello </p>
+            <p>World</p>
+            <p>!</p>
+        </div>            
+
+
+
+    </div>
+  )
+}
